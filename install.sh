4c6d6be9bea611cab7a5ede557e1d676404a51c1
okuno <okuno.mitsuhiro@moneyforward.co.jp>
2015-07-15 10:14:42 +0900

    add pry rspec

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-ln -s ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+
+ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+ln -sf ${HOME}/dotfiles/gemrc ${HOME}/.gemnrc
+ln -sf ${HOME}/dotfiles/pryrc ${HOME}/.pryrc
+ln -sf ${HOME}/dotfiles/rspec ${HOME}/.rspec

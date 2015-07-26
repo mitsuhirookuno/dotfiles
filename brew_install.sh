3b8596514522075ab84ec56f1896ab95b2b4854f
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2015-07-26 22:09:27 +0900

    add brew install.sh

---
+++ brew_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+brew install vim
+brew install byobu
+brew install ack
+brew install ag
+brew install tig
+brew install lv
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install firefox
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install sequel-pro
+brew cask install sophos-anti-virus-home-edition
+brew cask install iterm2
+brew cask install kobito
+brew cask install libreoffice
+brew cask install xtrafinder
+# https://ja.libreoffice.org/download/libreoffice-fresh/
+# 日本語化
+brew cask install sourcetree
+brew cask install alfred
+

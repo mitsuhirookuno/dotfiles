6a4f9afc9bd7bed6cd0ec72039bcd4b57f04a9e7
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2017-04-02 08:56:30 +0900

    古い記述から新しい記述へ

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
-ln -sf ${HOME}/dotfiles/gemrc ${HOME}/.gemnrc
+ln -sf ${HOME}/dotfiles/gemrc ${HOME}/.gemrc
 ln -sf ${HOME}/dotfiles/pryrc ${HOME}/.pryrc
 ln -sf ${HOME}/dotfiles/rspec ${HOME}/.rspec
 
